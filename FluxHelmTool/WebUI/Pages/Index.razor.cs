e090ff8b0c595e36a5f4fab1c9d5e8adf2b69e95
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2021-01-24 23:04:41 -0800

    wip

--- FluxHelmTool/WebUI/Pages/Index.razor.cs
+++ FluxHelmTool/WebUI/Pages/Index.razor.cs
@@ -1,26 +1,11 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Components;
-using Microsoft.MobileBlazorBindings;
-using Microsoft.MobileBlazorBindings.Elements;
-using Xamarin.Essentials;
-using Xamarin.Forms;
 using Agno.BlazorInputFile;
-using System.Net.Http.Json;
-using Microsoft.AspNetCore.Components.Forms;
-using Microsoft.AspNetCore.Components.Routing;
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.JSInterop;
-using FluxHelmTool.WebUI.Shared;
 using System.IO;
 using System.Net.Http;
-using System.Net;
 using YamlDotNet.RepresentationModel;
 using YamlDotNet.Serialization;
-using YamlDotNet.Serialization.NamingConventions;
-using FluxHelmTool;
 using BlazorMonacoYaml;
 using ICSharpCode.SharpZipLib.GZip;
 using ICSharpCode.SharpZipLib.Tar;
@@ -42,6 +27,10 @@ namespace FluxHelmTool.WebUI.Pages
 
         public string chartName;
 
+        public Dictionary<string, string> chartVersions = new Dictionary<string, string>();
+
+        public string selectedVersion;
+
         async Task HandleSelection(IFileListEntry[] files)
         {
             var file = files.FirstOrDefault();
@@ -54,7 +43,7 @@ namespace FluxHelmTool.WebUI.Pages
                 TextReader stringStream = new StreamReader(ms);
                 var yaml = new YamlStream();
                 yaml.Load(stringStream);
-
+                
                 left = yaml.Documents[0].RootNode;
 
                 var serializer = new SerializerBuilder().Build();
@@ -66,13 +55,15 @@ namespace FluxHelmTool.WebUI.Pages
                 await _yamlDiffEditor.SetOriginalValue(yamlStr.ToString());
 
                 GetChartInfo();
+
+                await GetChartVersions();
+
+                await GetRemoteChart();
             }
         }
 
         public void GetChartInfo()
         {
-            if (left == null) return;
-
             YamlMappingNode spec = ((YamlMappingNode)left).Children[new YamlScalarNode("spec")] as YamlMappingNode;
             YamlMappingNode chart = spec.Children[new YamlScalarNode("chart")] as YamlMappingNode;
 
@@ -84,15 +75,36 @@ namespace FluxHelmTool.WebUI.Pages
 
             var version = chart.Children[new YamlScalarNode("version")] as YamlScalarNode;
             chartVersion = version.Value;
+            selectedVersion = chartVersion;
+        }
+
+        async Task GetChartVersions()
+        {
+            var stream = await new HttpClient().GetStreamAsync(chartRepo.TrimEnd('/') + "/index.yaml");
+            var streamReader = new StreamReader(stream);
+            var yaml = new YamlStream();
+            yaml.Load(streamReader);
+
+            var mapping = yaml.Documents[0].RootNode as YamlMappingNode;
+
+            var items = (mapping.Children[new YamlScalarNode("entries")] as YamlMappingNode).Children[new YamlScalarNode(chartName)] as YamlSequenceNode;
+
+            chartVersions.Clear();
+
+            foreach (YamlNode item in items)
+            {
+                var version = (item[new YamlScalarNode("version")] as YamlScalarNode).Value;
+
+                var url = ((item[new YamlScalarNode("urls")] as YamlSequenceNode).Children.First() as YamlScalarNode).Value;
+                chartVersions.Add(version, url);
+            }
         }
 
         public async Task GetRemoteChart()
         {
-            //"https://charts.bitnami.com/bitnami/external-dns-4.5.4.tgz"
-
             var client = new HttpClient();
 
-            var stream = await client.GetStreamAsync(chartRepo.TrimEnd('/') + "/" + chartName + "-" + chartVersion + ".tgz");
+            var stream = await client.GetStreamAsync(chartVersions.GetValueOrDefault(selectedVersion));
 
             Stream gzipStream = new GZipInputStream(stream);
 
@@ -108,33 +120,12 @@ namespace FluxHelmTool.WebUI.Pages
                             tarInputStream.CopyEntryContents(fileContents);
                             fileContents.Position = 0;
                             TextReader stringStream = new StreamReader(fileContents);
-                            
+
                             await _yamlDiffEditor.SetModifiedValue(stringStream.ReadToEnd());
                         }
                         break;
                     }
                 }
-            }
-        }
-
-        async Task myButton()
-        {
-            HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Get, "https://charts.bitnami.com/bitnami/index.yaml");
-            HttpResponseMessage response = await new HttpClient().SendAsync(requestMessage, HttpCompletionOption.ResponseHeadersRead);
-            var stream = await response.Content.ReadAsStreamAsync();
-            Console.WriteLine("Response Length: " + response.Content.Headers.ContentLength);
-            var streamReader = new StreamReader(stream);
-            var yaml = new YamlStream();
-            yaml.Load(streamReader);
-
-            Console.WriteLine(yaml.Documents.Count);
-
-            // Examine the stream
-            var mapping = (YamlMappingNode)yaml.Documents[0].RootNode;
-
-            foreach (KeyValuePair<YamlNode, YamlNode> item in ((YamlMappingNode)mapping.Children[new YamlScalarNode("entries")]).Children)
-            {
-                //status += " - " + item.Children[new YamlScalarNode("name")];
             }
         }
     }
