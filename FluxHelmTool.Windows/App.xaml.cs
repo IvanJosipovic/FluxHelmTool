9fa0fb9d4518993edcca64c6173d237433844a09
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2021-02-04 19:10:59 -0800

    switched to WPF

---
+++ FluxHelmTool.Windows/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace FluxHelmTool.Windows
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
