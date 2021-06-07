dd69a493bd483f5927c2735ae9c3d54f6d388dcc
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2021-06-06 18:27:45 -0700

    updated to .net 6 preview

---
+++ FluxHelmTool.Windows/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿
+using Microsoft.AspNetCore.Components.WebView.WindowsForms;
+using Microsoft.Extensions.DependencyInjection;
+using System.Windows.Forms;
+
+namespace FluxHelmTool.Windows
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.blazorWebView1 = new Microsoft.AspNetCore.Components.WebView.WindowsForms.BlazorWebView();
+            this.SuspendLayout();
+            // 
+            // blazorWebView1
+            // 
+            this.blazorWebView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.blazorWebView1.Location = new System.Drawing.Point(0, 0);
+            this.blazorWebView1.Margin = new System.Windows.Forms.Padding(0);
+            this.blazorWebView1.Name = "blazorWebView1";
+            this.blazorWebView1.Size = new System.Drawing.Size(826, 474);
+            this.blazorWebView1.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(823, 474);
+            this.Controls.Add(this.blazorWebView1);
+            this.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            this.Name = "FluxHelmTool";
+            this.Text = "FluxHelmTool";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.AspNetCore.Components.WebView.WindowsForms.BlazorWebView blazorWebView1;
+    }
+}
---
+++ FluxHelmTool.Windows/Form1.cs
@@ -0,0 +1,32 @@
+﻿using FluxHelmTool.WebUI;
+using Microsoft.AspNetCore.Components.WebView.WindowsForms;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tewr.Blazor.FileReader;
+
+namespace FluxHelmTool.Windows
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddBlazorWebView();
+            serviceCollection.AddFileReaderService();
+
+            blazorWebView1.HostPage = @"wwwroot\index.html";
+            blazorWebView1.Services = serviceCollection.BuildServiceProvider();
+            blazorWebView1.RootComponents.Add<App>("#app");
+        }
+    }
+}
